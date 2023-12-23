49c9197dafa9db9daec17f3f1ca466300b66007e
bigOrange <1901346665@qq,com>
2023-12-23 16:31:42 +0800

    版本V1.2

--- js/myProject.js
+++ js/myProject.js
@@ -10,44 +10,30 @@ const rows = 10
 const cols = 8
 //格子大小
 const brickwidth = 50;
-//金币的个数
-let totalBricks = 20
 
 //定义图片数组
 let coinImages = document.querySelectorAll(".treasures img")
 //console.log(coinImages);
 
-//定义计数显示的数组
-let trLables = document.querySelectorAll('#treasures label')
-//console.log(trLables);
-
-//金币个数 数组  下标 代表 金币的类型
-let treasures = new Array()
-
 
 //定义历史记录显示对象
 let historyView = document.getElementById('recordItem');
 
-
-// 定义成绩显示
-let scoreView = document.getElementById("score")
-//定义绘制金币的位置
-//let goldBricks = []
-let goldBricks = new Array();
-//console.log(goldBricks);
-
-
 //已经实现的功能 开始和结束  随机关卡 能够进行 上下左右移动  
-//在遇到障碍物时能够判断并且不能移动到障碍物  拆弹功能已经实现
+//在遇到障碍物时能够判断并且不能移动到障碍物  拆弹功能已经实现 能够 实现本地存储 从而显示游戏记录
 
 let boy = new Array();
 
 let boom = new Array()
 //测试炸弹总数
 let totalBoom = 0
+
+// 定义成绩显示
+let scoreView = document.getElementById("score")
 //总得分
 let totalScore
 
+//定义计时显示
 let timeView = document.getElementById('timer');
 
 let timer = null;
@@ -74,13 +60,7 @@ function start() {
 
     document.getElementById('startBtn').disabled = true;
 
-    // //事件监听
-    // canvas.addEventListener('click', clickHandler)
-
-    // for (let i = 0; i < coinImages.length; i++) {
-    //     treasures[i] = 0;
-    // }
-    //重新设置
+    //重新设置初始化
     totalScore = 0
     totalBoom = 0
     boy.col = 0
@@ -242,14 +222,34 @@ function findNext(myboy, keyCode) {
 //拆弹事件
 function remove() {
     for (let i in boom) {
-        if (boom[i].row == boy.row && boom[i].col == boy.col && boom[i].sign == 1) {
+        if (boom[i].row == boy.row && boom[i].col == boy.col && boom[i].sign == 1 && boom[i].cnt == 0) {
             boom[i].cnt = 1;
             totalScore++;
             scoreView.innerHTML = totalScore
             drawinterface()
             if (totalBoom == 0) {
+                clearInterval(timer);
                 alert('炸弹已经拆除完毕')
-                stop()
+                //游戏记录
+                let dateTime = new Date();
+                let dtStr = dateTime.toLocaleString();
+                let successPrefix = '<label style="color: green">游戏成功';
+                let itemStr = successPrefix + dtStr +
+                    '，耗时 ' + time / 1000 + ' 秒 ' + '得分 ' + totalScore + '分' + '</label>';
+                // localStorage本地存储
+                let storage = window.localStorage
+                if (typeof (storage) == 'undefined') {
+                    alert("浏览器不支持LocalStorge")
+
+                    //直接写在页面上
+                    let span = document.createElement('span')
+                    span.innerHTML = itemStr
+                    document.getElementById('recordItem').append(span);
+
+                } else {
+                    historyStore(itemStr);
+                    historyView.innerHTML = storage.getItem('historyList');
+                }
             }
             break;
         }
@@ -263,139 +263,27 @@ function refresh() {
     timeView.innerHTML = time / 1000;
 }
 
+//历史记录
+function historyStore(info) {
+    let storage = window.localStorage
+    let history = ''
+    if (storage.getItem("historyList") == null) {
+        storage.setItem("historyList", info)
 
-// //事件处理 包括点击判断
-// function clickHandler() {
-//     //获取 相对坐标
-//     let x = event.offsetX
-//     let y = event.offsetY
-//     //计算点击的行列
-//     let col = Math.floor(x / brickwidth)
-//     let row = Math.floor(y / brickwidth)
+    } else {
+        history = storage.getItem("historyList") + '<br />'
+        history = history + info;
+        storage.setItem('historyList', history);
+    }
+}
 
-//     //判断是否点中金币的格子
-//     let brick = goldBricks[row]
-//     console.log(brick);
-//     if (brick !== null) {
-
-//         if (brick.col === col) {
-//             treasures[brick.coin]++;
-
-//             //显示
-//             console.log(trLables[brick.coin]);
-//             trLables[brick.coin].innerHTML = treasures[brick.coin]
-//             totalScore = 0;
-//             for (let i in treasures) {
-//                 totalScore += treasures[i] * (parseInt(i) + 1)
-//             }
-//             scoreView.innerHTML = totalScore
-
-//             //修改为 在任意 金币位置点击 都能进行 记分 以及 消除金币
-//             // if (row == rows - 1) {
-//             eraseLine(row)
-//             //}
-//         }
-//         else {
-//             overGame(0); //异常结束
-//         }
-//     }
-
-
-// }
-// //清除点击的金币  加入 row参数 确定 金币的位置
-// function eraseLine(row) {
-//     totalBricks--;
-
-//     clearBricks()
-
-//     //使用 splice 修改 金币数组  让 点击的金币 删除 然后再新添加
-//     goldBricks.splice(row, 1);
-
-//     if (totalBricks >= rows) {
-//         goldBricks.unshift(genBrick())
-//         console.log(goldBricks);
-
-//     }
-//     else {
-//         goldBricks.unshift(null)
-//     }
-//     if (totalBricks == 0)
-//         overGame(1);
-//     drawBricks()
-// }
-
-
-
-
-
-
-
-
-
-// //游戏结束
-// function overGame(flag) {
-//     if (flag) {
-//         alert('恭喜完成游戏！耗时' + time + '毫秒');
-//     } else {
-//         alert('你踩中了灰色格子，游戏失败！');
-//     }
-//     stop();
-
-//     //游戏记录
-//     let dateTime = new Date();
-//     let dtStr = dateTime.toLocaleString();
-//     let successPrefix = '<label style="color: green">游戏成功';
-//     let failPrefix = '<label style="color: red">游戏失败';
-//     let itemStr = (flag ? successPrefix : failPrefix) + dtStr +
-//         '，耗时 ' + time / 1000 + ' 秒</label>';
-
-//     //localStorage本地存储
-//     let storage = window.localStorage
-//     if (typeof (storage) == 'undefined') {
-//         alert("浏览器不支持LocalStorge")
-
-//         //直接写在页面上
-//         let span = document.createElement('span')
-//         span.innerHTML = itemStr
-//         document.getElementById('recordItem').append(span);
-
-//     } else {
-//         historyStore(itemStr);
-//         historyView.innerHTML = storage.getItem('historyList');
-//     }
-
-// }
-
-// function historyStore(info) {
-//     let storage = window.localStorage
-//     let history = ''
-//     if (storage.getItem("historyList") == null) {
-//         storage.setItem("historyList", info)
-
-//     } else {
-//         history = storage.getItem("historyList") + '<br />'
-//         history = history + info;
-//         storage.setItem('historyList', history);
-//     }
-// }
-
-
-// function clearBricks() {
-//     for (let i = 0; i < rows; i++) {
-//         if (goldBricks[i] != null) {
-//             let brick = goldBricks[i];
-//             ctx.clearRect((brick.col) * brickwidth + 1, i * brickwidth + 1, brickwidth - 2, brickwidth - 2)
-//         }
-//     }
-// }
-
-// //清除历史记录
-// function clearHistory() {
-//     historyView.innerHTML = '';
-//     if (!window.localStorage) {
-//         alert('浏览器不支持LocalStorge！');
-//         return;
-//     } else {
-//         window.localStorage.removeItem('historyList');
-//     }
-// }
+//清除历史记录
+function clearHistory() {
+    historyView.innerHTML = '';
+    if (!window.localStorage) {
+        alert('浏览器不支持LocalStorge！');
+        return;
+    } else {
+        window.localStorage.removeItem('historyList');
+    }
+}
